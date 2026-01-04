947d949546e25ae2d1dfc5feb0ed51bd454bb2ae
Gordon Watts <gwatts@uw.edu>
2026-01-04 15:07:34 -0800

    run proper installation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,9 @@
 import pytest
 
-from ami_helper.utils import ensure_and_import
-
-
 @pytest.fixture(autouse=True)
 def ensure_pyami_installed() -> None:
-    ensure_and_import("pyAMI_atlas")
+    from ami_helper.utils import ensure_setup
+    ensure_setup()
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
